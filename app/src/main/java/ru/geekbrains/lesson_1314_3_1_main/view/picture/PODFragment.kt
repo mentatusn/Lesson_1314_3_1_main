fb81bab1c8ee8bfe260b125bf2e73e76b00d62be
Andej Nesterenko <mentatus.n@gmail.com>
2021-09-06 23:19:55 +0300

    Разобрались с BottomSheet

--- app/src/main/java/ru/geekbrains/lesson_1314_3_1_main/view/picture/PODFragment.kt
+++ app/src/main/java/ru/geekbrains/lesson_1314_3_1_main/view/picture/PODFragment.kt
@@ -7,23 +7,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.geekbrains.lesson_1314_3_1_main.R
+import ru.geekbrains.lesson_1314_3_1_main.databinding.BottomSheetLayoutBinding
 import ru.geekbrains.lesson_1314_3_1_main.databinding.FragmentMainBinding
 import ru.geekbrains.lesson_1314_3_1_main.viewmodel.PODData
 import ru.geekbrains.lesson_1314_3_1_main.viewmodel.PODViewModel
 
 
-class PODFragment: Fragment() {
+class PODFragment : Fragment() {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     private var _binding: FragmentMainBinding? = null
-    val binding:FragmentMainBinding
-    get(){
-        return _binding!!
-    }
+    val binding: FragmentMainBinding
+        get() {
+            return _binding!!
+        }
 
     private val viewModel: PODViewModel by lazy {
         ViewModelProvider(this).get(PODViewModel::class.java)
@@ -46,22 +51,44 @@ class PODFragment: Fragment() {
         viewModel.sendServerRequest()
         binding.inputLayout.setEndIconOnClickListener {
             val i = Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+                data =
+                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
             }
-            startActivity( i)
+            startActivity(i)
         }
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.includeLayout.bottomSheetContainer)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        /*bottomSheetBehavior.addBottomSheetCallback(
+            object :
+                BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    when (newState) {
+                        BottomSheetBehavior.STATE_DRAGGING -> TODO("not implemented")
+                        BottomSheetBehavior.STATE_COLLAPSED -> TODO("not implemented")
+                        BottomSheetBehavior.STATE_EXPANDED -> TODO("not implemented")
+                        BottomSheetBehavior.STATE_HALF_EXPANDED -> TODO("not implemented")
+                        BottomSheetBehavior.STATE_HIDDEN -> TODO("not implemented")
+                        BottomSheetBehavior.STATE_SETTLING -> TODO("not implemented")
+                    }
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    TODO("not implemented")
+                }
+            }
+        )*/
     }
 
-    private fun renderData(data: PODData){
-        when(data){
+    private fun renderData(data: PODData) {
+        when (data) {
             is PODData.Error -> {//TODO HW
-                Toast.makeText(context,"PODData.Error",Toast.LENGTH_LONG).show()
-                }
+                Toast.makeText(context, "PODData.Error", Toast.LENGTH_LONG).show()
+            }
             is PODData.Loading -> {
-                Toast.makeText(context,"PODData.Loading",Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "PODData.Loading", Toast.LENGTH_LONG).show()
             }
             is PODData.Success -> {
-                binding.imageView.load(data.serverResponseData.url){ // квадратное становится прямоугольным
+                binding.imageView.load(data.serverResponseData.url) { // квадратное становится прямоугольным
                     error(R.drawable.ic_load_error_vector)
                 }
             }
@@ -73,7 +100,7 @@ class PODFragment: Fragment() {
         _binding = null
     }
 
-    companion object{
-        fun newInstance()= PODFragment()
+    companion object {
+        fun newInstance() = PODFragment()
     }
 }
