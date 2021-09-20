c081647487bd7f22e32510bc8f00d51b65a76b88
Andej Nesterenko <mentatus.n@gmail.com>
2021-09-20 22:09:40 +0300

    5 урок разобрали перемешивание

--- app/src/main/java/ru/geekbrains/lesson_1314_3_1_main/view/animations/AnimationsActivity.kt
+++ app/src/main/java/ru/geekbrains/lesson_1314_3_1_main/view/animations/AnimationsActivity.kt
@@ -9,8 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 
@@ -18,31 +20,32 @@ import ru.geekbrains.lesson_1314_3_1_main.R
 import ru.geekbrains.lesson_1314_3_1_main.databinding.ActivityAnimationsEnlargeBinding
 import ru.geekbrains.lesson_1314_3_1_main.databinding.ActivityAnimationsExplodeBinding
 import ru.geekbrains.lesson_1314_3_1_main.databinding.ActivityAnimationsPathTransitionsBinding
+import ru.geekbrains.lesson_1314_3_1_main.databinding.ActivityAnimationsShuffleBinding
 
 class AnimationsActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityAnimationsPathTransitionsBinding
+    lateinit var binding: ActivityAnimationsShuffleBinding
 
-    var isRightAnimation = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsPathTransitionsBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsShuffleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val titles:MutableList<String> = ArrayList()
+        for(i in 0..4){
+            titles.add("Item $i")
+        }
         binding.button.setOnClickListener {
-            isRightAnimation= !isRightAnimation
-            val changeBounds = ChangeBounds()
-            changeBounds.setPathMotion(ArcMotion())
-            changeBounds.duration = 2000
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,changeBounds)
-
-            val params =  binding.button.layoutParams as FrameLayout.LayoutParams
-            if(isRightAnimation){
-                params.gravity = Gravity.END or Gravity.BOTTOM
-            }else{
-                params.gravity = Gravity.TOP or Gravity.START
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer,ChangeBounds())
+            binding.transitionsContainer.removeAllViews()
+            titles.shuffle()
+            for(title in titles){
+                binding.transitionsContainer.addView(TextView(this).apply {
+                    text= title
+                    ViewCompat.setTransitionName(this,title)
+                    gravity = Gravity.CENTER_HORIZONTAL
+                })
             }
-            binding.button.layoutParams = params
-
         }
     }
 
